d55326ef74c5acc8097a9e263dd9491cda9f2f38
CervinoB <jcervinobarbosa@gmail.com>
2023-12-07 12:17:36 -0300

    change to nike

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,7 @@
 
 describe('template spec', () => {
-  beforeEach(() => {
-    cy.visit("https://www.saucedemo.com/");
-    cy.get('[className="login_logo"]').should('exist');
-    cy.get("#user-name").type("standard_user");
-    cy.get("#password").type("secret_sauce");
-  }),
-  
+
   it('', () => {
-    cy.contains('test').should('exist')
+    cy.visit("https://www.nike.com/");
   })
 })
