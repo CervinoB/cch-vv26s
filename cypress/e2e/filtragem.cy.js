850838ce9afd0ec2bb73fc5486f9ecd54ef5d754
gabrielznegri <gabrielznegri@gmail.com>
2023-12-08 22:41:39 -0300

    Ajustando filtragem

--- cypress/e2e/filtragem.cy.js
+++ cypress/e2e/filtragem.cy.js
@@ -1,6 +1,10 @@
 describe("Filtragem", () => {
-  it("Realizar a filtragem de produtos",()=>{
-    cy.visit('https://www.mercadolivre.com.br/');
-    cy.get('a[href="https://www.mercadolivre.com.br/ofertas#nav-header"]').click();
-  })
+  it("Aplicar filtros na pesquisa de produtos", () => {
+    cy.visit("https://www.mercadolivre.com.br/");
+    cy.get('[name="as_word"]').type("macbook");
+    cy.get(".nav-search-btn").click();
+    cy.url().should("include", "macbook");
+    cy.get(".ui-search-filter-highlighted").last().click();
+    cy.url().should("include", "CustoFrete_Gratis");
+  });
 });
--- cypress/e2e/pesquisa-de-produtos.cy.js
+++ cypress/e2e/pesquisa-de-produtos.cy.js
@@ -6,13 +6,4 @@ describe("Pesquisa de Produtos", () => {
     cy.url().should("include", "smartphone");
     cy.get(".ui-search-layout").should("have.length.greaterThan", 0);
   });
-
-  it("Aplicar filtros na pesquisa de produtos", () => {
-    cy.visit("https://www.mercadolivre.com.br/");
-    cy.get('[name="as_word"]').type("macbook");
-    cy.get(".nav-search-btn").click();
-    cy.url().should("include", "macbook");
-    cy.get(".ui-search-filter-highlighted").last().click();
-    cy.url().should("include", "CustoFrete_Gratis");
-  });
 });
